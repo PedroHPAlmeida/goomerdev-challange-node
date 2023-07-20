21304998c7c3e138c9f0959442019b4ec7c73f11
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-07-19 21:47:40 -0300

    fix: move arquivo daysOfWeek para o pacote utils

--- src/models/schedule.js
+++ src/models/schedule.js
@@ -1,5 +1,5 @@
 "use strict";
-const daysOfWeekEnum = require("./daysOfWeek");
+const daysOfWeekEnum = require("../utils/daysOfWeek");
 const daysOfWeek = daysOfWeekEnum.getValues();
 const {
 	Model
